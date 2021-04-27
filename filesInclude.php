adb68688034c252af7c7ab72edb21f6eed4ffe03
killas96 <60001978+killas96@users.noreply.github.com>
2021-04-27 16:15:32 +0500

    Update filesInclude.php

--- filesInclude.php
+++ filesInclude.php
@@ -1,7 +1,6 @@
 function filesInclude($dir, $pattern) {
 	$scan_dir = array_diff(scandir($dir), array('..', '.'));	
 	foreach($scan_dir as $res){
-		#echo '<pre style="display:none;">';print_r($dir . DIRECTORY_SEPARATOR . $res);echo '</pre>';
 		if (!is_dir($dir . DIRECTORY_SEPARATOR . $res) && stripos($res, $pattern) !== false)
 			require_once($dir . DIRECTORY_SEPARATOR . $res);
 	}
